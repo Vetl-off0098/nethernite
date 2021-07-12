717830743b8695f0333269bbbc46d5379a9c4a0f
Dmitriy <vetl-off@mail.ru>
2021-07-12 14:28:13 +0300

    Add most popular package load

--- src/store/index.js
+++ src/store/index.js
@@ -5,17 +5,30 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    popular: [],
     posts: {}
   },
   mutations: {
+    SET_POPULAR (state, popular) {
+      state.popular = popular
+    },
     SET_POSTS (state, posts) {
       state.posts = posts
     }
   },
   actions: {
-    async fetchPosts ({ commit }) {
+    async fetchPopular ({ commit }) {
       try {
-        let response = await fetch('https://jsonplaceholder.typicode.com/posts')
+        let response = await fetch('https://data.jsdelivr.com/v1/stats/packages')
+        response = await response.json()
+        commit('SET_POPULAR', response)
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    async fetchPosts ({ commit }, param) {
+      try {
+        let response = await fetch(`https://jsonplaceholder.typicode.com/${param}`)
         response = await response.json()
         commit('SET_POSTS', response)
       } catch (err) {
@@ -26,6 +39,9 @@ export default new Vuex.Store({
   getters: {
     getPosts: state => {
       return state.posts
+    },
+    getPopular: state => {
+      return state.popular
     }
   },
   modules: {
